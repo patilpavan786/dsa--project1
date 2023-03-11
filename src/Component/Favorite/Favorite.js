5a2b32174b917d9e2e9e52ce19a232356421f183
patilpavan786 <pavan.p.b.pavan@gmail.com>
2023-03-11 15:44:34 +0530

    delete added

--- src/Component/Favorite/Favorite.js
+++ src/Component/Favorite/Favorite.js
@@ -1,68 +1,79 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../Atom/Navbar/Navbar";
 import CustomButton from "../../Atom/CustomButton/CustomButton";
 import { useNavigate } from "react-router-dom";
 import style from "./Favorite.module.css";
 
-
 export default function Favorite() {
-  const previousContacts = JSON.parse(localStorage.getItem('favoritePackage')) || [];
-const[show,setShow]=useState(false)
-console.log(previousContacts)
+  const previousContacts = JSON.parse(localStorage.getItem("favoritePackage")) || [];
 
-useEffect(()=>{
-if(previousContacts.length>0){
-  setShow(true)
-}
-},[previousContacts])
+  const [show, setShow] = useState(false);
+  const [allContacts, setAllContacts] = useState(previousContacts);
+  console.log(previousContacts);
 
+  useEffect(() => {
+    if (previousContacts.length > 0) {
+      setShow(true);
+    }
+  }, [previousContacts]);
 
+  const handleDelete = (e) => {
+    const contacts = JSON.parse(localStorage.getItem("favoritePackage")) || [];
+    const otherContacts = contacts.filter((contact) => contact.value1 !== e);
+    localStorage.setItem("favoritePackage", JSON.stringify(otherContacts));
+    setAllContacts(otherContacts);
+  };
 
-  const navigate= useNavigate()
+  const navigate = useNavigate();
   return (
-    <div >
+    <div>
       <Navbar />
       <div className={style.title}>
         <h1>Welcome to Favorite NPM packages</h1>
-    { show? <> <CustomButton 
-         txt="Add fav"
-         onClick={()=> navigate("/")}
-         className={style.btn}
-        
-        /></> : null  }
+        {show ? (
+          <>
+            <CustomButton
+              txt="Add fav"
+              onClick={() => navigate("/")}
+              className={style.btn}
+            />
+          </>
+        ) : null}
       </div>
 
-    { show? <>
-    
-    <div>
-{previousContacts.map((x)=>{return(
- 
- <div className={style.cardmain}>
- <div className={style.card}>
-  <p>{x.value1}</p>
-  <p>{x.value2}</p>
-  <CustomButton txt='Edit'  className={style.btn}/>
-  <CustomButton txt='Delete'  className={style.btn}/>
-  </div>
-  </div>
-)})}
-
-
-    </div>
-    
-    
-    
-    
-    </> : <div className={style.mainbox}>
-        <div className={style.box}>
-          <h3>You dont have any Fav yet , Please Add</h3>
-          <CustomButton
-            txt="Add fav"
-            onClick={()=> navigate("/")}
-            className={style.btn}
-          />
+      {show ? (
+        <>
+          <div>
+            {allContacts.map((x) => {
+              return (
+                <div className={style.cardmain}>
+                  <div className={style.card}>
+                    <p>{x.value1}</p>
+                    <p>{x.value2}</p>
+                    <CustomButton txt="Edit" className={style.btn} />
+                    <CustomButton
+                      txt="Delete"
+                      onClick={() => handleDelete(x.value1)}
+                      className={style.btn}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <div className={style.mainbox}>
+          <div className={style.box}>
+            <h3>You dont have any Fav yet , Please Add</h3>
+            <CustomButton
+              txt="Add fav"
+              onClick={() => navigate("/")}
+              className={style.btn}
+            />
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 }
